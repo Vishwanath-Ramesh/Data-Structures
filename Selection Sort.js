d9e2fdcddc96521f8cee32cd318e93d43fbcac05
Vishwanath-Ramesh <54787778+Vishwanath-Ramesh@users.noreply.github.com>
2019-09-17 06:12:39 +0530

    Update Selection Sort.js

--- Selection Sort.js
+++ Selection Sort.js
@@ -1,1 +1,1 @@
-
+/*Pick an element at the very beginning and compare it with the other elements. If it's lower than the picked one swap those and continue picking the next element until all the elements are sorted.*/
