4e812173e173452f58e87e18091c600e55143e76
Vishwanath-Ramesh <54787778+Vishwanath-Ramesh@users.noreply.github.com>
2019-09-17 06:24:19 +0530

    Algorithm - Insertion Sort

---
+++ Insertion Sort.js
@@ -0,0 +1,7 @@
+/*Starting with the first element, place the element to its correct position by comparing it with the previous elements and continue with the next element.
+  
+Ex :  5 3 1 8 9
+1.Place 5
+2.Compare 3 with 5 and move 3 to the 1st position.
+3.Compare 1 with 3 & 5, and move 1 to 1st position.
+4.Go on.*/ 
