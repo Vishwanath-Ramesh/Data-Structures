78b63b6952b7ae413a702d66f5fde71f1872ae3b
Vishwanath R <vishwanath@LogicValley.in>
2020-10-10 15:03:06 +0530

    Bubble & Selection Sort - Notes update

--- Bubble Sort.js
+++ Bubble Sort.js
@@ -1,1 +1,7 @@
-/*Compare each and every element by looping through*/
+/*Compare each and every element starting from the beginning by looping through and swap them to the left until all the elements are sorted completely*/
+
+Eg : 8 5 2 9 5 6 3 
+1. Compare 8 with 5, since 8 is greater, swap them.
+2. Compare 2nd element(8) with 2, since 8 is greater, swap them.
+3. Compare 3rd element(8) with 9, since 8 is smaller than 9, so move on to the next one.
+4. Swap until all the elements are sorted completely.
--- Selection Sort.js
+++ Selection Sort.js
@@ -1,1 +1,131 @@
-/*Pick an element at the very beginning and compare it with the other elements. If it's lower than the picked one swap those and continue picking the next element until all the elements are sorted.*/
+/* Pick an element at the very beginning and compare it with the other elements. 
+If it's lower than the picked one swap those and continue picking the next element until all the elements are sorted. */
+
+function selectionSort(inputArray) {
+  if (inputArray.length === 0) return inputArray;
+
+  var pointer = 0,
+    sortedElements = 0,
+    ctr = 0,
+    tempValue = null;
+
+  while (sortedElements < inputArray.length) {
+    if (inputArray[ctr] < inputArray[pointer]) pointer = ctr;
+
+    if (ctr === inputArray.length - 1) {
+      tempValue = inputArray[sortedElements];
+      inputArray[sortedElements] = inputArray[pointer];
+      inputArray[pointer] = tempValue;
+      sortedElements++;
+      pointer = sortedElements;
+      ctr = pointer;
+    } else ctr++;
+  }
+  return inputArray;
+}
+
+const result = selectionSort([
+  100,
+  4,
+  17,
+  91,
+  56,
+  82,
+  92,
+  72,
+  59,
+  69,
+  18,
+  2,
+  96,
+  3,
+  88,
+  24,
+  28,
+  81,
+  96,
+  80,
+  76,
+  43,
+  36,
+  60,
+  29,
+  100,
+  33,
+  38,
+  71,
+  98,
+  97,
+  57,
+  92,
+  88,
+  37,
+  48,
+  46,
+  38,
+  54,
+  65,
+  52,
+  34,
+  70,
+  82,
+  54,
+  77,
+  11,
+  80,
+  38,
+  87,
+  12,
+  81,
+  95,
+  12,
+  61,
+  57,
+  61,
+  26,
+  64,
+  70,
+  40,
+  84,
+  20,
+  83,
+  64,
+  44,
+  90,
+  14,
+  34,
+  82,
+  67,
+  54,
+  50,
+  23,
+  86,
+  72,
+  67,
+  31,
+  12,
+  15,
+  90,
+  61,
+  32,
+  18,
+  17,
+  59,
+  39,
+  76,
+  57,
+  22,
+  81,
+  95,
+  82,
+  30,
+  18,
+  40,
+  25,
+  11,
+  90,
+  39,
+  9,
+  60,
+]);
+console.log(result.join(" "));
